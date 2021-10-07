220df903f0f0b3522c0678cfe6cb2abdf8428808
Col0ring <1561999073@qq.com>
2021-10-08 02:20:50 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import type { Component } from 'solid-js'
-import logo from './logo.svg'
-import styles from './App.module.css'
+import BaseDemo from './components/base'
+import ControlFlowDemo from './components/control-flow'
+import DemoRouter from './components/router'
+import TodoList from './components/todo-list'
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <BaseDemo />
+      <ControlFlowDemo />
+      <TodoList />
+      <DemoRouter />
+    </>
   )
 }
 
